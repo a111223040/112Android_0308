01e6d9e15235fc2b4119466a813cf58b4e152651
a111223040 <a111223040@mail.shu.edu.tw>
2024-03-22 15:01:23 +0800

    2024-03-22

--- UI_Test2/app/build.gradle.kts
+++ UI_Test2/app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ui_test2"
+    namespace = "com.example.ui_test"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.ui_test2"
+        applicationId = "com.example.ui_test"
         minSdk = 30
         targetSdk = 34
         versionCode = 1
